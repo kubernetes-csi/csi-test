c6abd2baa9536cf01722107a454c78e93b4866c4
Luis Pabón <luis@portworx.com>
2018-01-19 14:45:20 -0500

    sanity: Create a real mountpoint

--- hack/e2e.sh
+++ hack/e2e.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+UDS="/tmp/e2e-csi-sanity.sock"
 CSI_ENDPOINTS="tcp://127.0.0.1:9998"
-CSI_ENDPOINTS="$CSI_ENDPOINTS /tmp/e2e-csi-sanity.sock"
-CSI_ENDPOINTS="$CSI_ENDPOINTS unix:///tmp/e2e-csi-sanity.sock"
+CSI_ENDPOINTS="$CSI_ENDPOINTS unix://${UDS}"
+CSI_ENDPOINTS="$CSI_ENDPOINTS ${UDS}"
 
 go get -u github.com/thecodeteam/gocsi/mock
 cd cmd/csi-sanity
@@ -10,19 +11,14 @@ cd cmd/csi-sanity
 cd ../..
 
 for endpoint in $CSI_ENDPOINTS ; do
-	if ! echo $endpoint | grep tcp > /dev/null 2>&1 ; then
-		rm -f $endpoint
-	fi
+    rm -f $UDS
 
 	CSI_ENDPOINT=$endpoint mock &
 	pid=$!
 
 	csi-sanity $@ --ginkgo.skip=MOCKERRORS --csi.endpoint=$endpoint ; ret=$?
 	kill -9 $pid
-
-	if ! echo $endpoint | grep tcp > /dev/null 2>&1 ; then
-		rm -f $endpoint
-	fi
+    rm -f $UDS
 
 	if [ $ret -ne 0 ] ; then
 		exit $ret
