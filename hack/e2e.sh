721c80f75faf11a8e5b08f3375862379891715da
Luis Pabón <luis@portworx.com>
2018-01-19 08:49:16 -0500

    sanity: Update e2e
    Signed-off-by: Luis Pabón <luis@portworx.com>

--- hack/e2e.sh
+++ hack/e2e.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 CSI_ENDPOINTS="tcp://127.0.0.1:9998"
-#CSI_ENDPOINTS="$CSI_ENDPOINTS /tmp/csi.sock"
-#CSI_ENDPOINTS="$CSI_ENDPOINTS unix:///tmp/csi.sock"
+CSI_ENDPOINTS="$CSI_ENDPOINTS /tmp/csi.sock.$$"
+CSI_ENDPOINTS="$CSI_ENDPOINTS unix:///tmp/csi.sock.$$"
+CSI_ENDPOINTS="$CSI_ENDPOINTS csi.sock.$$"
 
-if [ ! -x $GOPATH/bin/mock ] ; then
-	go get -u github.com/thecodeteam/gocsi/mock
-fi
-
+go get -u github.com/thecodeteam/gocsi/mock
 cd cmd/csi-sanity
   make clean install || exit 1
 cd ../..
 
 for endpoint in $CSI_ENDPOINTS ; do
+	if ! echo $endpoint | grep tcp > /dev/null 2>&1 ; then
+		rm -f $endpoint
+	fi
+
 	CSI_ENDPOINT=$endpoint mock &
 	pid=$!
 
@@ -21,6 +23,10 @@ for endpoint in $CSI_ENDPOINTS ; do
 
 	if [ $ret -ne 0 ] ; then
 		exit $ret
+	fi
+
+	if ! echo $endpoint | grep tcp > /dev/null 2>&1 ; then
+		rm -f $endpoint
 	fi
 done
 
