f703beb8eb0c20ff5f0ba6df66261d1f473e4b58
edisonxiang <xiang.edison@gmail.com>
2018-01-24 15:30:00 +0800

    Add test for NodeGetCapabilities

---
+++ pkg/sanity/node.go
@@ -0,0 +1,74 @@
+/*
+Copyright 2017 Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package sanity
+
+import (
+	"fmt"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/container-storage-interface/spec/lib/go/csi"
+	context "golang.org/x/net/context"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("NodeGetCapabilities [Node Server]", func() {
+	var (
+		c csi.NodeClient
+	)
+
+	BeforeEach(func() {
+		c = csi.NewNodeClient(conn)
+	})
+
+	It("should fail when no version is provided", func() {
+		_, err := c.NodeGetCapabilities(
+			context.Background(),
+			&csi.NodeGetCapabilitiesRequest{})
+		Expect(err).To(HaveOccurred())
+
+		serverError, ok := status.FromError(err)
+		Expect(ok).To(BeTrue())
+		Expect(serverError.Code()).To(Equal(codes.InvalidArgument))
+	})
+
+	It("should return appropriate capabilities", func() {
+		caps, err := c.NodeGetCapabilities(
+			context.Background(),
+			&csi.NodeGetCapabilitiesRequest{
+				Version: csiClientVersion,
+			})
+
+		By("checking successful response")
+		Expect(err).NotTo(HaveOccurred())
+		Expect(caps).NotTo(BeNil())
+		Expect(caps.GetCapabilities()).NotTo(BeNil())
+
+		for _, cap := range caps.GetCapabilities() {
+			Expect(cap.GetRpc()).NotTo(BeNil())
+
+			switch cap.GetRpc().GetType() {
+			case csi.NodeServiceCapability_RPC_UNKNOWN:
+			default:
+				Fail(fmt.Sprintf("Unknown capability: %v\n", cap.GetRpc().GetType()))
+			}
+		}
+	})
+})
