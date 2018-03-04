d8a662489c827a87bb0f64d11a8816d370e3413b
Luis Pabón <luis@portworx.com>
2018-03-04 17:04:44 -0500

    Report correct exit status

--- mock/main.go
+++ mock/main.go
@@ -1,3 +1,18 @@
+/*
+Copyright 2018 Kubernetes Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
 import (
@@ -16,11 +31,11 @@ func main() {
 	endpoint := os.Getenv("CSI_ENDPOINT")
 	if len(endpoint) == 0 {
 		fmt.Println("CSI_ENDPOINT must be defined and must be a path")
-		return
+		os.Exit(1)
 	}
 	if strings.Contains(endpoint, ":") {
 		fmt.Println("CSI_ENDPOINT must be a unix path")
-		return
+		os.Exit(1)
 	}
 
 	// Create mock driver
@@ -39,7 +54,7 @@ func main() {
 		fmt.Printf("Error: Unable to listen on %s socket: %v\n",
 			endpoint,
 			err)
-		return
+		os.Exit(1)
 	}
 	defer os.Remove(endpoint)
 
@@ -47,6 +62,7 @@ func main() {
 	if err := d.Start(l); err != nil {
 		fmt.Printf("Error: Unable to start mock CSI server: %v\n",
 			err)
+		os.Exit(1)
 	}
 	fmt.Println("mock driver started")
 
