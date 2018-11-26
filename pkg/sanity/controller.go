3eb09f475a82b97075a18f287b88d8bce7505c3c
Humble Devassy Chirammal <humble.devassy@gmail.com>
2018-11-26 13:41:43 +0530

    Add ControllerServiceCapability_RPC_CLONE_VOLUME to GetCapabilities.

--- pkg/sanity/controller.go
+++ pkg/sanity/controller.go
@@ -125,6 +125,7 @@ var _ = DescribeSanity("Controller Service", func(sc *SanityContext) {
 				case csi.ControllerServiceCapability_RPC_CREATE_DELETE_SNAPSHOT:
 				case csi.ControllerServiceCapability_RPC_LIST_SNAPSHOTS:
 				case csi.ControllerServiceCapability_RPC_PUBLISH_READONLY:
+				case csi.ControllerServiceCapability_RPC_CLONE_VOLUME:
 				default:
 					Fail(fmt.Sprintf("Unknown capability: %v\n", cap.GetRpc().GetType()))
 				}
