237339f110ae50692a4e34ad51caf1b4e59c448c
Patrick Ohly <patrick.ohly@intel.com>
2019-04-05 08:39:07 +0200

    .prow.sh: test against a known-good hostpath driver
    For csi-test, we need to lock down testing onto a driver that is known
    to pass. Testing different versions of the driver with csi-sanity is
    done in the csi-driver-host-path repo.

--- .prow.sh
+++ .prow.sh
@@ -1,6 +1,16 @@
 #! /bin/bash -e
 
-CSI_PROW_TESTS="unit sanity"
+# A Prow job can override these defaults, but this shouldn't be necessary.
+
+# Only these tests make sense for csi-sanity.
+: ${CSI_PROW_TESTS:="unit sanity"}
+
+# What matters for csi-sanity is the version of the hostpath driver
+# that we test against, not the version of Kubernetes that it runs on.
+# We pick the latest stable Kubernetes here because the corresponding
+# deployment has the current driver. v1.0.1 (from the current 1.13
+# deployment) does not pass csi-sanity testing.
+: ${CSI_PROW_KUBERNETES_VERSION:=1.14.0}
 
 . release-tools/prow.sh
 
