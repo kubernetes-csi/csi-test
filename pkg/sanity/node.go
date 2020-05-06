396075f022dbccecf0114d941cdb7b3548740bef
Nicolas Trangez <ikke@nicolast.be>
2020-05-06 10:02:37 +0000

    sanity: make `NodePublishVolume` test more generic
    The input validation tests for the `NodePublishVolume` RPC (and others)
    depends on the ordering by which a CSI node plugin performs the
    validation checks. This was hidden because all checks tested for return
    the same error. However, when adding a test which expects a different
    error code, this becomes an issue.
    
    Instead of constructing 'mostly-invalid' requests in many of the tests,
    this patch changes the strategy to construct a *valid* request by
    default, then in every test only switch the field under test to some
    invalid value, then asserting the expected error code is returned.

--- pkg/sanity/node.go
+++ pkg/sanity/node.go
@@ -346,13 +346,34 @@ var _ = DescribeSanity("Node Service", func(sc *TestContext) {
 	})
 
 	Describe("NodePublishVolume", func() {
+		var req *csi.NodePublishVolumeRequest
+
+		BeforeEach(func() {
+			stagingTargetPath := ""
+			if nodeStageSupported {
+				stagingTargetPath = sc.StagingPath
+			}
+
+			req = &csi.NodePublishVolumeRequest{
+				VolumeId:          sc.Config.IDGen.GenerateUniqueValidVolumeID(),
+				StagingTargetPath: stagingTargetPath,
+				TargetPath:        sc.TargetPath + "/target",
+				VolumeCapability:  TestVolumeCapabilityWithAccessType(sc, csi.VolumeCapability_AccessMode_SINGLE_NODE_WRITER),
+				Readonly:          false,
+				Secrets:           sc.Secrets.NodePublishVolumeSecret,
+			}
+		})
+
+		JustAfterEach(func() {
+			if CurrentGinkgoTestDescription().Failed {
+				fmt.Fprintf(GinkgoWriter, "Request was %v\n", req)
+			}
+		})
+
 		It("should fail when no volume id is provided", func() {
-			_, err := c.NodePublishVolume(
-				context.Background(),
-				&csi.NodePublishVolumeRequest{
-					Secrets: sc.Secrets.NodePublishVolumeSecret,
-				},
-			)
+			req.VolumeId = ""
+
+			_, err := c.NodePublishVolume(context.Background(), req)
 			Expect(err).To(HaveOccurred())
 
 			serverError, ok := status.FromError(err)
@@ -361,13 +382,9 @@ var _ = DescribeSanity("Node Service", func(sc *TestContext) {
 		})
 
 		It("should fail when no target path is provided", func() {
-			_, err := c.NodePublishVolume(
-				context.Background(),
-				&csi.NodePublishVolumeRequest{
-					VolumeId: sc.Config.IDGen.GenerateUniqueValidVolumeID(),
-					Secrets:  sc.Secrets.NodePublishVolumeSecret,
-				},
-			)
+			req.TargetPath = ""
+
+			_, err := c.NodePublishVolume(context.Background(), req)
 			Expect(err).To(HaveOccurred())
 
 			serverError, ok := status.FromError(err)
@@ -376,15 +393,9 @@ var _ = DescribeSanity("Node Service", func(sc *TestContext) {
 		})
 
 		It("should fail when no volume capability is provided", func() {
-			_, err := c.NodePublishVolume(
-				context.Background(),
-				&csi.NodePublishVolumeRequest{
-					VolumeId:         sc.Config.IDGen.GenerateUniqueValidVolumeID(),
-					VolumeCapability: nil,
-					TargetPath:       sc.TargetPath + "/target",
-					Secrets:          sc.Secrets.NodePublishVolumeSecret,
-				},
-			)
+			req.VolumeCapability = nil
+
+			_, err := c.NodePublishVolume(context.Background(), req)
 			Expect(err).To(HaveOccurred())
 
 			serverError, ok := status.FromError(err)
@@ -393,20 +404,9 @@ var _ = DescribeSanity("Node Service", func(sc *TestContext) {
 		})
 
 		It("should fail when the volume is missing", func() {
-			stagingPath := ""
-			if nodeStageSupported {
-				stagingPath = sc.StagingPath
-			}
+			req.VolumeId = sc.Config.IDGen.GenerateUniqueValidVolumeID()
 
-			_, err := c.NodePublishVolume(
-				context.Background(),
-				&csi.NodePublishVolumeRequest{
-					VolumeId:          sc.Config.IDGen.GenerateUniqueValidVolumeID(),
-					VolumeCapability:  TestVolumeCapabilityWithAccessType(sc, csi.VolumeCapability_AccessMode_SINGLE_NODE_WRITER),
-					StagingTargetPath: stagingPath,
-					TargetPath:        sc.TargetPath + "/target",
-					Secrets:           sc.Secrets.NodePublishVolumeSecret,
-				})
+			_, err := c.NodePublishVolume(context.Background(), req)
 			Expect(err).To(HaveOccurred())
 
 			serverError, ok := status.FromError(err)
